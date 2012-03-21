a3074f615e7b917934c4cb4999c0ae287f2549cc
Max Degterev <max@ostrovok.ru>
2012-03-21 18:43:18 +0400

    diff structure

--- datepicker.js
+++ datepicker.js
@@ -1,3 +1,6 @@
+// ==================================================================
+// INIT SECTION
+// ==================================================================
 var DatePicker = function(container, options) {
     this.container = (typeof container === 'string') ? $(container) : container;
 
@@ -8,17 +11,24 @@ var DatePicker = function(container, options) {
     this.options = $.extend({
         selectors: {
             monthsHolder: '.dp-months-holder',
+
+            calWrap: '.dp-cal-wrap',
+
             calHolder: '.dp-cal-holder',
+            calControls: '.dp-cal-controls',
+
             calHandles: '.dp-cal-handle',
-            arrivalBlock: '.dp-cal-arrival-area',
-            departureBlock: '.dp-cal-departure-area',
-            arrivalHandle: '.dp-cal-arrival-handle',
-            departureHandle: '.dp-cal-departure-handle'
+            lArea: '.dp-cal-left-area',
+            rArea: '.dp-cal-right-area',
+            mArea: '.dp-cal-middle-area',
+
+            lHandle: '.dp-cal-left-handle',
+            rHandle: '.dp-cal-right-handle'
         },
         months: 12,
         defaultNights: 4
     }, options);
-    
+
     this.profiler = {
         start: +(new Date())
     }
@@ -29,6 +39,8 @@ DatePicker.prototype.locale = {
                    'октябрь', 'ноябрь', 'декабрь']
 };
 DatePicker.prototype.init = function() {
+    console.log('init: ' + ((+(new Date()) - this.profiler.start)));
+
     this.state = {};
     this.els = {};
     this.sizes = {};
@@ -38,35 +50,34 @@ DatePicker.prototype.init = function() {
     }
 
     this.now = new Date();
-    this.today = new Date(this.now.getFullYear(), this.now.getMonth(), this.now.getDate());
     this.start = new Date(this.now.getFullYear(), this.now.getMonth(), this.now.getDate());
     this.end = new Date(this.now.getFullYear(), this.now.getMonth() + this.options.months, this.now.getDate());
     
-    console.log('init: ' + ((+(new Date()) - this.profiler.start)));
-    
+
     this.generateLabels();
-    
     console.log('labels: ' + ((+(new Date()) - this.profiler.start)));
+
     this.generateCalendar();
     console.log('calendar: ' + ((+(new Date()) - this.profiler.start)));
     
-    this.els.cells = this.els.calendar.children('.calendar-day');
-    
-    this.resetHolder();
+    this.getSizes();
 
     this.logic();
 };
+// ==================================================================
+// GENERATION SEKSHEN
+// ==================================================================
 DatePicker.prototype.generateLabels = function() {
-    var monthLabels = '<table class="calendar-month-labels"><tr>',
+    var monthLabels = '<ul class="calendar-month-labels">',
         curr = this.start.getMonth();
     
     for (var i = 0; i < this.options.months; i++) {
-        monthLabels += '<td>' + this.locale.month_labels[curr] + '<\/td>';
+        monthLabels += '<li>' + this.locale.month_labels[curr] + '<\/li>';
         curr = (curr === 11) ? 0 : (curr + 1);
     }
-    
-    monthLabels += '<\/tr><\/table>';
-    
+
+    monthLabels += '<\/ul>';
+
     this.els.monthsHolder.append(monthLabels);
 };
 DatePicker.prototype.generateCalendar = function() {
@@ -84,114 +95,128 @@ DatePicker.prototype.generateCalendar = function() {
         monthlabels: true,
         type: 'list'
     }));
- 
-    this.els.calHolder.prepend(this.els.calendar);
+
+    this.els.calHolder.html(this.els.calendar);
+    this.els.cells = this.els.calendar.children('.calendar-day');
 };
-DatePicker.prototype.resetHolder = function() {
+DatePicker.prototype.getSizes = function() {
     this.sizes.cell = this.els.cells.eq(0).outerWidth();
     this.sizes.calendar = this.sizes.cell * this.els.cells.length;
     
     this.sizes.offset = this.els.calHolder.offset().left;
 
     this.els.calendar.css({ width: this.sizes.calendar });
-    this.els.arrivalBlock.css({ width: this.sizes.calendar });
-    this.els.departureBlock.css({ width: this.sizes.calendar });
 };
-DatePicker.prototype.resetOffset = function() {
-    this.sizes.offset = this.els.calHolder.offset().left;
-};
+// ==================================================================
+// LOGYK SECTION
+// ==================================================================
 DatePicker.prototype.logic = function() {
-    this.els.calendar.on('click', '.calendar-day', $.proxy(this.handleCellsClick, this));
-    this.els.calHandles.on('mousedown', $.proxy(this.handleDragBlock, this));
-    $(window).on('resize', $.proxy(this.resetOffset, this));
+    this.mainLogic();
+    this.calendarLogic();
+    this.controlsLogic();
 };
-DatePicker.prototype.handleCellsClick = function(e) {
-    var el = $(e.currentTarget),
-        date = el.data('date');
-
-    if (!this.state.checkin && !this.state.checkout) {
-        var dateO = this.YMDToDate(date),
-            checkout = this.dateToYMD(new Date(dateO.getFullYear(), dateO.getMonth(), dateO.getDate() + this.options.defaultNights));
-        
-        // First click
-        this.state.checkin = date;
-        this.state.checkout = checkout;
-
-        this.els.calHolder.addClass('controls');
-        this.setPosFromDates();
-    }
-    else {
-        // n-th click. probably won't have any
-        
-    }
+DatePicker.prototype.mainLogic = function() {
+    var that = this;
+    var resetOffset = function() {
+        that.sizes.offset = that.els.calHolder.offset().left;
+    };
+    $(window).on('resize', resetOffset);
 };
-DatePicker.prototype.handleDragBlock = function(e) {
+DatePicker.prototype.calendarLogic = function() {
+    var that = this;
+    var handleCellsClick = function(e) {
+        var el = $(this),
+            date = el.data('date'),
+            dateO = that.YMDToDate(date),
+            ndate = that.dateToYMD(new Date(dateO.getFullYear(), dateO.getMonth(), dateO.getDate() + that.options.defaultNights));
+
+        that.state.lDate = date;
+        that.state.rDate = ndate;
+
+        that.els.calWrap.addClass('controls');
+        that.setPosFromDates();
+        
+        this.els.calendar.off('click', '.calendar-day', handleCellsClick);
+    };
+    this.els.calendar.on('click', '.calendar-day', handleCellsClick);
+};
+
+DatePicker.prototype.controlsLogic = function() {
     var that = this,
         doc = $(document),
-        el = $(e.currentTarget),
-        left = el.hasClass('dp-cal-arrival-handle'),
         renderT = +(new Date()),
         len = that.els.cells.length;
-    
-    var handleDragMove = function(e) {
-        var now = +(new Date());
-        if (now - renderT < 30) {
-            return;
-        }
 
-        var x = e.pageX - that.sizes.offset,
-            i, pos;
+    var handleDragStart = function(e) {
+        var el = $(this),
+            left = el.hasClass('dp-cal-left-handle');
+    
+        var handleDragMove = function(e) {
+            var now = +(new Date());
+            if (now - renderT < 30) {
+                return;
+            }
+
+            renderT = now;
+
+            var x = e.pageX - that.sizes.offset,
+                i, pos;
         
-        if (left) {
-            i = Math.min(that.state.depH - 1, Math.max(1, Math.ceil(x / that.sizes.cell)));
-        }
-        else {
-            i = Math.min(len, Math.max(that.state.arrH + 1, Math.ceil(x / that.sizes.cell)));
-        }
+            if (left) {
+                i = Math.min(that.state.rHandle - 1, Math.max(1, Math.ceil(x / that.sizes.cell)));
+            }
+            else {
+                i = Math.min(len, Math.max(that.state.lHandle + 1, Math.ceil(x / that.sizes.cell)));
+            }
             
-        pos = that.sizes.cell * i - that.sizes.cell / 2;
-        renderT = now;
+            pos = that.sizes.cell * i - that.sizes.cell / 2;
 
-        if (left) {
-            that.state.arrH = i;
-        }
-        else {
-            that.state.depH = i;
-        }
+            if (left) {
+                that.state.lHandle = i;
+            }
+            else {
+                that.state.rHandle = i;
+            }
+
+            that.setHandlePos();
+        };
+    
+        var handleDragEnd = function() {
+            doc.off('mousemove.datepicker', handleDragMove);
+            doc.off('mouseup.datepicker', handleDragEnd);
         
-        that.setHandlePos();
+            that.setDatesFromPos();
+        
+            that.els.calWrap.removeClass('dragging');
+        };
+
+        doc.on('mousemove.datepicker', handleDragMove);
+        doc.on('mouseup.datepicker', handleDragEnd);
+
+        that.els.calWrap.addClass('dragging');
     };
-    
-    var handleDragEnd = function() {
-        doc.off('mousemove.datepicker', handleDragMove);
-        doc.off('mouseup.datepicker', handleDragEnd);
-        
-        that.setDatesFromPos();
-        
-        that.els.calHolder.removeClass('dragging');
-    };
-    
-    doc.on('mousemove.datepicker', handleDragMove);
-    doc.on('mouseup.datepicker', handleDragEnd);
-    
-    this.els.calHolder.addClass('dragging');
+
+    this.els.calHandles.on('mousedown', handleDragStart);
 };
+// ==================================================================
+// UTILITY SECTION
+// ==================================================================
 DatePicker.prototype.setPosFromDates = function() {
-    var checkin = this.els.cells.filter('[data-date="' + this.state.checkin + '"]'),
-        checkout = this.els.cells.filter('[data-date="' + this.state.checkout + '"]');
+    var lDate = this.els.cells.filter('[data-date="' + this.state.lDate + '"]'),
+        rDate = this.els.cells.filter('[data-date="' + this.state.rDate + '"]');
         
-    this.state.arrH = checkin.index() + 1;
-    this.state.depH = checkout.index() + 1;
+    this.state.lHandle = lDate.index() + 1;
+    this.state.rHandle = rDate.index() + 1;
 
     this.setHandlePos();
 };
 DatePicker.prototype.setHandlePos = function() {
-    this.els.arrivalBlock.css({ left: this.sizes.cell * this.state.arrH - this.sizes.cell / 2 - this.sizes.calendar });
-    this.els.departureBlock.css({ left: this.sizes.cell * this.state.depH - this.sizes.cell / 2 });
+    this.els.lArea.css({ left: this.sizes.cell * this.state.lHandle - this.sizes.cell / 2 - this.sizes.calendar });
+    this.els.rArea.css({ left: this.sizes.cell * this.state.rHandle - this.sizes.cell / 2 });
 };
 DatePicker.prototype.setDatesFromPos = function() {
-    this.state.checkin = this.els.cells.eq(this.state.arrH - 1).data('date');
-    this.state.checkout = this.els.cells.eq(this.state.depH - 1).data('date');
+    this.state.lDate = this.els.cells.eq(this.state.lHandle - 1).data('date');
+    this.state.rDate = this.els.cells.eq(this.state.rHandle - 1).data('date');
 };
 DatePicker.prototype.dateToYMD = function(date) {
     return date.getFullYear() + '-' + ('0' + (date.getMonth() + 1)).slice(-2) + '-' + ('0' + date.getDate()).slice(-2);
